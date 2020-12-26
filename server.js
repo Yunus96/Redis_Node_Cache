29f9c303ff80520af78d495a88d9c8db13655840
Mohammad Yunus <mohammadyunus102@gmail.com>
2020-12-26 15:44:22 +0530

    add server.js

--- server.js
+++ server.js
@@ -9,16 +9,34 @@ const client = redis.createClient(REDIS_PORT)
 
 const app = express();
 
+function setResponse(username, repo){
+    return `<h2>${username} has ${repo} github repos</h2>`
+}
+
 //make request to github for data
 async function getRepose(req, res, next){
     try{
         console.log('getting data from github')
+        const { username } = req.params;
+
+        const response = await fetch(`https://api.github.com/users/${username}`)
+        const data = await response.json();
+
+       
+        const repos = data.public_repos;
+
+        // Set data to redis
+        client.setex(username, 3600, repos)
+
+        res.send(setResponse(username, repos))
+
+        
     } catch (err) {
         console.error(err);
         res.status(500)
     }
 }
 
-app.get('/repos/:username' getRepose)
+app.get('/repos/:username', getRepose)
 
 app.listen(PORT, ()=> console.log( `server listening on ${PORT}`))
